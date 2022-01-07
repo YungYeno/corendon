81025a03339038380502596e61bc5dadb4369593
Yeno <43321063+YungYeno@users.noreply.github.com>
2022-01-07 17:57:33 +0100

    Updated installer

--- project/install.py
+++ project/install.py
@@ -1,3 +1,4 @@
+# Author - Yeno Reus, IC106E
 import os
 from colorama import init, Fore
 
@@ -10,6 +11,7 @@ MAGENTA = Fore.MAGENTA
 WHITE = Fore.WHITE
 
 
+# Super cool intro :)
 def intro():
     print(f"""{MAGENTA}
 ███████ ██    ██ ███████     ██ ███    ██ ███████ ████████  █████  ██      ██      ███████ ██████  
@@ -19,58 +21,66 @@ def intro():
 ██         ██    ███████     ██ ██   ████ ███████    ██    ██   ██ ███████ ███████ ███████ ██   ██ """)
 
 
+# Install the needed requirements
 def requirements():
-    print(f"{GREEN}\nInstalling the project requirements...")
+    print(f"{GREEN}\nInstalling the project requirements...{WHITE}")
     command = "sudo apt install git python3-flask python3-flask-login python3-flask-sqlalchemy"
     try:
-        os.system(command)
+        result = os.system(command)
+        print(result)
     except OSError as error:
         print(f"{RED}\nSomething went wrong with installing requirements! : {WHITE}{error}")
 
 
+# Clone the github project
 def get_project():
-    print(f"{GREEN}\nDownloading project files...")
+    print(f"{GREEN}\nDownloading project files...{WHITE}")
     command = "git clone https://github.com/YungYeno/corendon.git"
     try:
-        os.system(command)
+        result = os.system(command)
+        print(result)
     except OSError as error:
         print(f"{RED}\nSomething went wrong with getting project files : {error}")
 
 
+# Move files to proper location
 def moving_files():
-    print(f"{GREEN}\nAdding files to the wsgi folder...")
-    commands = ["sudo mv project /var/www/fys",
-                "cd /var/www/fys",
-                "sudo mkdir corendon",
-                "cd project",
-                "sudo mv app.py app.wsgi auth.py db.sqlite __init__.py models.py requirements.txt static templates /var/www/fys/wsgi",
-                "sudo mv corendon.conf /etc/apache2/sites-available"]
+    path = "corendon/project/"
+    print(f"{GREEN}\nAdding files to the wsgi folder...{WHITE}")
+    commands = ["sudo mv project /var/www/fys/wsgi",
+                "sudo mkdir /var/www/fys/corendon",
+                f"sudo mv {path}*.html {path}db.sqlite {path}requirements.txt {path}static {path}templates /var/www/fys/wsgi/",
+                f"sudo mv {path}corendon.conf /etc/apache2/sites-available"]
     try:
         for command in commands:
-            os.system(command)
+            result = os.system(command)
+            print(result)
     except OSError as error:
         print(f"{RED}\nSomething went wrong trying to move the files: {error}")
 
 
+# Set the right permissions
 def permissions():
-    print(f"{GREEN}\nSetting permissions...")
-    commands = ["sudo chmod 755 -R /var/www/fys",
-                "sudo chmod 663 /etc/apache2/sites-available/corendon.conf"]
+    print(f"{GREEN}\nSetting permissions...{WHITE}")
+    commands = ["sudo chmod 777 -R /var/www/fys/wsgi",
+                "sudo chmod 777 /etc/apache2/sites-available/corendon.conf"]
     try:
         for command in commands:
-            os.system(command)
+            result = os.system(command)
+            print(result)
     except OSError as error:
         print(f"{RED}\nSomething went wrong setting the permissions : {error}")
 
 
+# Let user know that script is completed.
 def outro():
     print(f"{GREEN}\nScript completed! {WHITE}Restart apache2 with {MAGENTA}'systemctl restart apache2'")
 
 
 if __name__ == '__main__':
     intro()
-    requirements()
-    get_project()
-    moving_files()
-    permissions()
-    outro()
+    # requirements()
+    # get_project()
+    # moving_files()
+    # permissions()
+    # outro()
